15e9b50a60084ec3d72b9ac4e3f92aaf32e4ef95
Mark Anthony <markanthonyfamarin@gmail.com>
2025-05-03 19:31:42 +0800

    feat: developed the sidebar for web and mobile, and layout for the pages

--- app/routes/admin/all-users.tsx
+++ app/routes/admin/all-users.tsx
@@ -1,3 +1,13 @@
+import { Header } from "components";
+
 export default function AllUsers() {
-	return <div>All Users</div>;
+	return (
+		<main className="dashboard wrapper">
+			<Header
+				title={"Trips Page"}
+				description="Check out our current users in real time"
+			/>
+			All Users Page Contents
+		</main>
+	);
 }
--- app/routes/admin/dashboard.tsx
+++ app/routes/admin/dashboard.tsx
@@ -1,3 +1,14 @@
+import { Header } from "components";
+
 export default function Dashboard() {
-	return <div>Dashboard</div>;
+	const user = { name: "Adrian" };
+	return (
+		<main className="dashboard wrapper">
+			<Header
+				title={`Welcome ${user?.name ?? "Guest"} 👋`}
+				description="Track activity, trends and popular destinations in real time"
+			/>
+			Dashboard Page Contents
+		</main>
+	);
 }
